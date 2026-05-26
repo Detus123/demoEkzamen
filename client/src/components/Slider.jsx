d7e752873ce3b9f5550344a2d2446fa0f5777ba2
Detus123 <detusevamaria@gmail.com>
2026-05-26 10:53:35 +0300

    Update Slider.jsx

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import img1 from '../assets/1.jpg'
 import img2 from '../assets/2.jpg'
 import img3 from '../assets/3.jpg'
-import img4 from '../assets/4.webp'
+import img4 from '../assets/4.jpg'
 const images = [img1, img2, img3, img4]
 export default function Slider() {
   const [curr, setCurr] = useState(0)
